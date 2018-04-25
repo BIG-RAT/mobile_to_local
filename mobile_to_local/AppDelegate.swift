023e6138b6ca00e0b843187259ea69f119585337
Leslie Helou <leslie@ccs-indy.com>
2018-04-25 10:54:12 -0400

    Initial Commit

---
+++ mobile_to_local/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mobile_to_local
+//
+//  Created by Leslie Helou on 4/25/18.
+//  Copyright © 2018 jamf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mobile_to_local/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mobile_to_local
+//
+//  Created by Leslie Helou on 4/25/18.
+//  Copyright © 2018 jamf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
