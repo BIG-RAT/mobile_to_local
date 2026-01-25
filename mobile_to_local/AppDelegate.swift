491a2e58df695389011289ba686388f79836abbb
Leslie Helou <leslie@ccs-indy.com>
2026-01-24 21:13:19 -0500

    prevent switching keyboard layout

--- mobile_to_local/AppDelegate.swift
+++ mobile_to_local/AppDelegate.swift
@@ -12,8 +12,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         configureTelemetryDeck()
+        
+        // Prevent keyboard layout switching
+        setupInputSourceHandling()
     }
 
+    private func setupInputSourceHandling() {
+        // Disable automatic input source switching
+        let workspace = NSWorkspace.shared
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(applicationActivated),
+            name: NSWorkspace.didActivateApplicationNotification,
+            object: workspace
+        )
+    }
+    
+    @objc private func applicationActivated(_ notification: Notification) {
+        // Ensure we don't force keyboard change
+        if let textInputContext = NSTextInputContext.current {
+            textInputContext.invalidateCharacterCoordinates()
+        }
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
